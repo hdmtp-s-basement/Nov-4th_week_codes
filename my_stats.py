dd706e6090d4ecf0fbf8b399dd9e6fd33ec8c06c
hDmtP <pritamdhara914@gmail.com>
2021-11-22 23:45:52 +0530

    added hr:mn:s:ms

--- my_stats.py
+++ my_stats.py
@@ -4,7 +4,7 @@ import datetime
 user = GithubUser('hdmtp')
 contribs = user.contributions()
 
-contribs_2021 = user.contributions(start_date='2021-11-22', end_date=str(datetime.date.today()))
+contribs_2021 = user.contributions(start_date='2021-11-22', end_date=str(datetime.datetime.now()))
 #print(sum([day.count for day in contribs_2021.days]))
 
 
