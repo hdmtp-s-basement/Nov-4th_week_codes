b8825307f9ea6f7d3342f10ebb9057c887de981f
hDmtP <pritamdhara914@gmail.com>
2021-11-22 23:34:47 +0530

    testing 1

---
+++ my_stats.py
@@ -0,0 +1,18 @@
+from github_contributions import GithubUser
+import datetime
+
+user = GithubUser('hdmtp')
+contribs = user.contributions()
+
+contribs_2021 = user.contributions(start_date='2021-11-22', end_date=str(datetime.date.today()))
+#print(sum([day.count for day in contribs_2021.days]))
+
+
+sc = f'''
+**{datetime.date.today()}** | **{sum([day.count for day in contribs_2021.days])}**
+     '''
+
+f = open("README.md", "a")
+f.write(sc)
+f.close()
+
