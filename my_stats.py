9940a647ad20ed609c2e2c7809a838407d808a94
hDmtP <pritamdhara914@gmail.com>
2021-11-23 20:54:36 +0530

    testing 5

--- my_stats.py
+++ my_stats.py
@@ -8,7 +8,7 @@ contribs_2021 = user.contributions(start_date='2021-11-22', end_date=str(datetim
 #print(sum([day.count for day in contribs_2021.days]))
 
 
-sc = f"**{datetime.date.today()}** | **{sum([day.count for day in contribs_2021.days])}**"
+sc = f"**{datetime.datetime.now()}** | **{sum([day.count for day in contribs_2021.days])}**"
 
 f = open("README.md", "a")
 f.write(sc)
