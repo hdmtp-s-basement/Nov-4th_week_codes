0ca11e4d63dad564ef6b5ce06a6bd4b63ddd11e3
hDmtP <pritamdhara914@gmail.com>
2021-11-24 21:11:24 +0530

    explicit mentioning of timezone in my_stats.py

--- my_stats.py
+++ my_stats.py
@@ -1,10 +1,13 @@
 from github_contributions import GithubUser
 import datetime
+import pytz 
+   
+tz_NY = pytz.timezone('Asia/Kolkata')
 
 user = GithubUser('hdmtp')
 contribs = user.contributions()
 
-contribs_2021 = user.contributions(start_date='2021-11-22', end_date=str(datetime.datetime.now()))
+contribs_2021 = user.contributions(start_date='2021-11-22', end_date=str(datetime.now(tz_NY)))
 #print(sum([day.count for day in contribs_2021.days]))
 
 
