bffccce1582af630e585cea7245ca49850a73948
hDmtP <pritamdhara914@gmail.com>
2021-11-24 21:37:00 +0530

    redited timezone settingd in my_stats.py again

--- my_stats.py
+++ my_stats.py
@@ -8,11 +8,9 @@ datetime_NY = datetime.now(tz_NY)
 user = GithubUser('hdmtp')
 contribs = user.contributions()
 
-contribs_2021 = user.contributions(start_date='2021-11-22', end_date=datetime_NY.strftime("%Y-%m-%d %H:%M:%S.%f"))
-#print(sum([day.count for day in contribs_2021.days]))
+contribs_2021 = user.contributions(start_date='2021-11-22', end_date=datetime.date.today())
 
-
-sc = "\n"+f"**{datetime.datetime.now()}** | **{sum([day.count for day in contribs_2021.days])}**"
+sc = "\n"+f"**{datetime_NY.strftime("%Y-%m-%d %H:%M:%S.%f")}** | **{sum([day.count for day in contribs_2021.days])}**"
 
 f = open("README.md", "a")
 f.write(sc)
