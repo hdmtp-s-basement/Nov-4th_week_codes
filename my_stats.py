2e006a95d3572da0489dc3cabdb1c8735cb28773
hDmtP <pritamdhara914@gmail.com>
2021-11-24 21:31:38 +0530

    redited timezone settingd in my_stats.py

--- my_stats.py
+++ my_stats.py
@@ -1,13 +1,14 @@
 from github_contributions import GithubUser
-import datetime
-import pytz 
-   
-tz_NY = pytz.timezone('Asia/Kolkata')
+from datetime import datetime    
+import pytz    
+
+tz_NY = pytz.timezone('Asia/Kolkata')   
+datetime_NY = datetime.now(tz_NY)  
 
 user = GithubUser('hdmtp')
 contribs = user.contributions()
 
-contribs_2021 = user.contributions(start_date='2021-11-22', end_date=str(datetime.now(tz_NY)))
+contribs_2021 = user.contributions(start_date='2021-11-22', end_date=datetime_NY.strftime("%Y-%m-%d %H:%M:%S.%f"))
 #print(sum([day.count for day in contribs_2021.days]))
 
 
